b6a92f89c7ce9118d1c473734eaea2d137f88bec
Prawee Wongsa <prawee@hotmail.com>
2015-07-28 04:02:09 +0700

    update main layout with countdown

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,3 +1,35 @@
 <?php
+use yii\helpers\Html;
 use prawee\metroui\assets\MetroUiAsset;
 MetroUiAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<div class="wrap">
+    <div class="container">
+        <?= $content ?>
+        <div class="countdown" data-role="countdown" data-days="2"></div>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
