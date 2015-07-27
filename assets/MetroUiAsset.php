901648fb07f90c837ef89801877bf1b98ad55c98
Prawee Wongsa <prawee@hotmail.com>
2015-07-28 03:28:42 +0700

    add assets/MetroUiAsset

---
+++ assets/MetroUiAsset.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.yiiassets.com
+ * @copyright Copyright (c) 2015 Prawee Wongsa <konkeanweb@gmail.com>
+ * @license BSD-3-Clause
+ */
+namespace prawee\metroui\assets;
+
+use yii\web\AssetBundle;
+
+
+/**
+ * Asset bundel for the Metro UI css,js files.
+ * @author Prawee Wongsa <konkeanweb@gmail.com
+ * @since 1.0
+ */
+class MetroUiAsset extends AssetBundle{
+    public $sourcePath='@bower/metro-ui-css/build';
+    public $css=[
+        'css/metro.min.css',
+    ];
+    public $js=[
+        'js/metro.min.js',
+    ];
+    public $depends=[];
+}
